6afde185da88801d04d2f7056eae41b0141e7cfd
lopessoftec <lopessoftec@gmail.com>
2019-08-28 22:22:06 -0300

    desenvolvido like e dislike ao clicar nos botões e irá sumindo os perfis clicado

--- frontend/src/pages/Main.js
+++ frontend/src/pages/Main.js
@@ -1,6 +1,8 @@
 //realizar chamada a api assim que nosso componente for exibido em tela
 //toda vez que precisa ter uma variavel que vai ser manipulada pelo nosso componente
 import React, { useEffect, useState } from 'react';
+// serve para a gente criar uma ancora
+import { Link } from 'react-router-dom';
 import './Main.css';
 
 import api from '../services/api';
@@ -12,9 +14,9 @@ import like from '../assets/like.svg';
 //com esse match tenho todos os paramentros passado nessa rota
 export default function Main({ match }) {
     //inicialiando a variavel users com um array vazio useState([])
-    //função setusers não preciso criar essa função
+    //função setUsers não preciso criar essa função
     //quando mudamos um estado ele faz toda uma rederização do html
-    const [users, setusers] = useState([]);
+    const [users, setUsers] = useState([]);
 
     //primeira parte é qual função quer executar e a segunda parte é quando irei executar
     //posso passar variaveis dentro do array e toda vez que nossa função for alterada será chamado novamente o useEffect
@@ -28,7 +30,7 @@ export default function Main({ match }) {
                     user: match.params.id,
                 }
             });
-            setusers(response.data);
+            setUsers(response.data);
         }
 
         loadUsers();
@@ -37,37 +39,55 @@ export default function Main({ match }) {
     //função reposnavel pelo like
     //toda função que é gerada a apartir de uma interação com usario ele utiliza o handle...
     async function handleLike(id){
-        console.log('like', id);
+        await api.post(`/devs/${ id }/likes`, null, {
+            headers: { user: match.params.id },
+        });
+
+        setUsers(users.filter(user => user._id !== id));
     }
     async function handleDislike(id){
-        console.log('dislike', id);
+        await api.post(`/devs/${ id }/dislikes`, null, {
+            headers: { user: match.params.id },
+        });
+
+        // temos que atualizar a página apos realizar o dislike
+        // semore tenho que sobrescrever o valor da variavel users utilizando o setUsers
+        setUsers(users.filter(user => user._id !== id));
     }
 
     return (
         <div className="main-container">
-            <img src={logo} alt="Tindev" />
-            <ul>
-                {/* map serve para percorrer um array */}
-                {/* retorno user => ( */}
-                {users.map(user => (
-                    <li key={user._id}>
-                        <img src={user.avatar} alt={user.name} />
-                        <footer>
-                            <strong>{user.name}</strong>
-                            <p>{user.bio}</p>
-                        </footer>
-                        <div className="buttons">
-                            {/* assim que atualiza a pagina ele ja chama a função do onclick entao temos que criar outra função para da certo */}
-                            <button type="button" onClick={() => handleDislike(user._id)}>
-                                <img src={dislike} alt="Dislike" />
-                            </button>
-                            <button type="button">
-                                <img src={like} alt="Like" />
-                            </button>
-                        </div>
-                    </li>
-                ))}
-            </ul>
+            {/* to representa qual rota quero colocar aqui */}
+            <Link to="/">
+                <img src={logo} alt="Tindev" />
+            </Link>
+                {/* aqui possui um if */}
+                { users.length > 0 ? (
+                    <ul>
+                        {/* map serve para percorrer um array */}
+                        {/* retorno user => ( */}
+                        {users.map(user => (
+                            <li key={user._id}>
+                                <img src={user.avatar} alt={user.name} />
+                                <footer>
+                                    <strong>{user.name}</strong>
+                                    <p>{user.bio}</p>
+                                </footer>
+                                <div className="buttons">
+                                    {/* assim que atualiza a pagina ele ja chama a função do onclick entao temos que criar outra função para da certo */}
+                                    <button type="button" onClick={() => handleDislike(user._id)}>
+                                        <img src={dislike} alt="Dislike" />
+                                    </button>
+                                    <button type="button" onClick={() => handleLike(user._id)}>
+                                        <img src={like} alt="Like" />
+                                    </button>
+                                </div>
+                            </li>
+                        ))}
+                    </ul>
+                ) : (
+                    <div className="empty">Acabou :(</div>
+                ) }
         </div>
     )
     // e temos que colocar o nome do parametro da rota
