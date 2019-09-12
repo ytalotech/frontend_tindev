bb52654c88087724685e0fecbcc8644fb835984e
lopessoftec <lopessoftec@gmail.com>
2019-09-11 23:19:18 -0300

    trabalhando com socket para apresentar as mensagens de match no front

--- frontend/src/pages/Main.js
+++ frontend/src/pages/Main.js
@@ -1,6 +1,7 @@
 //realizar chamada a api assim que nosso componente for exibido em tela
 //toda vez que precisa ter uma variavel que vai ser manipulada pelo nosso componente
 import React, { useEffect, useState } from 'react';
+import io from 'socket.io-client';
 // serve para a gente criar uma ancora
 import { Link } from 'react-router-dom';
 import './Main.css';
@@ -10,6 +11,7 @@ import api from '../services/api';
 import logo from '../assets/logo.svg';
 import dislike from '../assets/dislike.svg';
 import like from '../assets/like.svg';
+import itsamatch from '../assets/itsamatch.pnp';
 
 //com esse match tenho todos os paramentros passado nessa rota
 export default function Main({ match }) {
@@ -17,6 +19,9 @@ export default function Main({ match }) {
     //função setUsers não preciso criar essa função
     //quando mudamos um estado ele faz toda uma rederização do html
     const [users, setUsers] = useState([]);
+
+    //criar um estado para saber quando deu metch ou não
+    const [matchDev, setMatchDev] = useState(true);
 
     //primeira parte é qual função quer executar e a segunda parte é quando irei executar
     //posso passar variaveis dentro do array e toda vez que nossa função for alterada será chamado novamente o useEffect
@@ -35,6 +40,34 @@ export default function Main({ match }) {
 
         loadUsers();
     }, [match.params.id]);
+
+    //vai se conectar com nosso websocket, mas poderia ser tudo em uma useEffect
+    useEffect(() => {
+
+        //irei conectar com bakend atraveis da variavel socket
+        //passo o ip do backend
+        const socket = io('http://localhost:3333', {
+            query: { user: match.params.id }
+        });
+        
+        //irei ouvir
+        socket.on('match', dev => {
+            console.log(dev);
+        });
+
+        // //preciso ouvir do backend a mensagem
+        // socket.on('world', message => {
+        //     console.log(message);
+        // })
+
+        // setTimeout(() => {
+        //     //estou emitindo para obackend uma menagem do tipo Hello. E possui um objeto com uma chave mesage e valor Hello Word
+        //     socket.emit('hello', {
+        //         message: 'Hello Word'
+        //     })
+        // }, 3000);
+    }, [match.params.id]);
+    //vai se cronixar toda vez que tiver um [match.params.id] diferente
 
     //função reposnavel pelo like
     //toda função que é gerada a apartir de uma interação com usario ele utiliza o handle...
@@ -87,6 +120,17 @@ export default function Main({ match }) {
                     </ul>
                 ) : (
                     <div className="empty">Acabou :(</div>
+                ) }
+                {/* se tiver alguma informação dentro de matchDev ele vai entrar */}
+                { matchDev && (
+                    <div className="match-containe">
+                        <img src={itsamatch} alt="It´s a match" />
+                        <img className="avatar" src="https://avatars1.githubusercontent.com/u/54129629?v=4" alt="" />
+                        <strong>Ytalo Lopes</strong>
+                        <p>CEO na Lopessoftec. Desenvolvedor web em PHP e aprendendo Javascript, reactJs, React Native, NodeJS,... Sempre em busca de novos conhecimentos.</p>
+
+                        <button type="button">FECHAR</button>
+                    </div>
                 ) }
         </div>
     )
